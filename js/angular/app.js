acdbf1afc8317a6be336e4c6947d3478da750632
VenkatapathiRajuM <mvpspl619@gmail.com>
2014-07-22 22:45:13 +0530

    Added initial routing

--- js/angular/app.js
+++ js/angular/app.js
@@ -6,9 +6,25 @@ angular.module('mmts', ['ngRoute'])
 		templateUrl: 'templates/default.html',
 		controller: 'defaultController'
 	})
+	.when('/searchtrains', {
+		templateUrl: 'templates/searchtrains.html',
+		controller: 'searchTrainsController'
+	})
+	.when('/livetrains', {
+		templateUrl: 'templates/livetrains.html',
+		controller: 'liveTrainController'
+	})
 	.otherwise('/');
 });
 
 angular.module('mmts').controller('defaultController', ['$scope', function($scope){
+	$scope.message = "Chrome Extensions welcomes AngularJS";
+}]);
+
+angular.module('mmts').controller('searchTrainsController', ['$scope', function($scope){
+	$scope.message = "Chrome Extensions welcomes AngularJS";
+}]);
+
+angular.module('mmts').controller('liveTrainController', ['$scope', function($scope){
 	$scope.message = "Chrome Extensions welcomes AngularJS";
 }]);
