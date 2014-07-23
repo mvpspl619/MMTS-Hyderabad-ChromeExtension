f771cd0e2f3fd731763a43a86d998405ab5e35f2
VenkatapathiRajuM <mvpspl619@gmail.com>
2014-07-23 11:14:38 +0530

    Populating the dropdowns with station data.

--- js/angular/app.js
+++ js/angular/app.js
@@ -17,8 +17,15 @@ angular.module('mmts', ['ngRoute'])
 	.otherwise('/');
 });
 
-angular.module('mmts').controller('defaultController', ['$scope', function($scope){
+angular.module('mmts').controller('defaultController', ['$scope', 'stationsService', function($scope, stationsService){
 	$scope.message = "Chrome Extensions welcomes AngularJS";
+	$scope.stations = [{"name": "junk", "value":0}, {"name": "waste", "value":1}]
+	var stationsPromise = stationsService.getStations();
+	stationsPromise.then(function(data){
+		$scope.stations = data
+	}, function(error){
+		console.log('An error occured while reading the file')
+	});
 }]);
 
 angular.module('mmts').controller('searchTrainsController', ['$scope', function($scope){
@@ -27,4 +34,14 @@ angular.module('mmts').controller('searchTrainsController', ['$scope', function(
 
 angular.module('mmts').controller('liveTrainController', ['$scope', function($scope){
 	$scope.message = "Chrome Extensions welcomes AngularJS";
+}]);
+
+angular.module('mmts').service('stationsService', ['$http', '$q', function($http, $q){
+	return {
+		getStations: function(){
+			var def = $q.defer();
+			$http.get('./data/stations.json').success(function(data){def.resolve(data)}).error(function(error){def.reject(error)});
+			return def.promise;
+		}
+	}
 }]);
