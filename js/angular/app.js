037e044d25563e6f091aa22fed726a09267c7397
VenkatapathiRajuM <mvpspl619@gmail.com>
2014-07-25 21:59:48 +0530

    Changes to search page

--- js/angular/app.js
+++ js/angular/app.js
@@ -17,15 +17,48 @@ angular.module('mmts', ['ngRoute'])
 	.otherwise('/');
 });
 
-angular.module('mmts').controller('defaultController', ['$scope', 'stationsService', function($scope, stationsService){
+angular.module('mmts').controller('defaultController', ['$scope', 'stationsService', 'sharedService', function($scope, stationsService, sharedService){
+	$scope.station = {};
 	$scope.message = "Chrome Extensions welcomes AngularJS";
 	$scope.stations = [{"name": "junk", "value":0}, {"name": "waste", "value":1}]
 	var stationsPromise = stationsService.getStations();
 	stationsPromise.then(function(data){
-		$scope.stations = data
+		$scope.stations = data;
+		InitializeData();
 	}, function(error){
 		console.log('An error occured while reading the file')
 	});
+	function InitializeData() {
+		if(sharedService.fromStation != null || sharedService.toStation != null) {
+			for (var i = $scope.stations.length - 1; i >= 0; i--) {
+				if(sharedService.fromStation != null){
+					if($scope.stations[i].value === sharedService.fromStation.value){
+						$scope.station.from = $scope.stations[i];
+					}
+				}
+				if(sharedService.toStation != null){
+					if($scope.stations[i].value === sharedService.toStation.value){
+						$scope.station.to = $scope.stations[i];
+					}	
+				}				
+			};	
+		}
+	}	
+	$scope.updateStation = function(type, stationModel){
+		switch(type) {
+		    case "from":
+		        sharedService.fromStation = stationModel;
+		        break;
+		    case "to":
+		        sharedService.toStation = stationModel;
+		        break;
+		}
+	}
+	$scope.interchange = function(){
+		var temp = $scope.station.from;
+		$scope.station.from = $scope.station.to;
+		$scope.station.to = temp;
+	}
 }]);
 
 angular.module('mmts').controller('searchTrainsController', ['$scope', function($scope){
@@ -45,3 +78,10 @@ angular.module('mmts').service('stationsService', ['$http', '$q', function($http
 		}
 	}
 }]);
+
+angular.module('mmts').service('sharedService', function(){
+	return {
+		fromStation: null,
+		toStation: null
+	}
+});
