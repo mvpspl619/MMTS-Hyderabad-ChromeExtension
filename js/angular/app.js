a1fa8fd5964a3616ee280c7e251edf86a1b2624f
VenkatapathiRajuM <mvpspl619@gmail.com>
2014-07-29 17:04:30 +0530

    Saving selected stations to localstorage

--- js/angular/app.js
+++ js/angular/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('mmts', ['ngRoute'])
+angular.module('mmts', ['ngRoute', 'LocalStorageModule'])
 .config(function($routeProvider){
 	$routeProvider.when('/', {
 		templateUrl: 'templates/default.html',
@@ -17,12 +17,14 @@ angular.module('mmts', ['ngRoute'])
 	.otherwise('/');
 });
 
-angular.module('mmts').controller('defaultController', ['$scope', 'stationsService', 'sharedService', function($scope, stationsService, sharedService){
+angular.module('mmts').controller('defaultController', ['$scope', 'stationsService', 'sharedService', '$location',
+ function($scope, stationsService, sharedService, $location){
 	$scope.station = {};
 	$scope.station.isValid = false;
 	var stationsPromise = stationsService.getStations();
 	stationsPromise.then(function(data){
 		$scope.stations = data;
+		sharedService.stations = data;
 		InitializeData();
 	}, function(error){
 		console.log('An error occured while reading the file')
@@ -31,12 +33,12 @@ angular.module('mmts').controller('defaultController', ['$scope', 'stationsServi
 		if(sharedService.fromStation != null || sharedService.toStation != null) {
 			for (var i = $scope.stations.length - 1; i >= 0; i--) {
 				if(sharedService.fromStation != null){
-					if($scope.stations[i].value === sharedService.fromStation.value){
+					if($scope.stations[i].value == sharedService.fromStation){
 						$scope.station.from = $scope.stations[i];
 					}
 				}
 				if(sharedService.toStation != null){
-					if($scope.stations[i].value === sharedService.toStation.value){
+					if($scope.stations[i].value == sharedService.toStation){
 						$scope.station.to = $scope.stations[i];
 					}	
 				}
@@ -55,10 +57,10 @@ angular.module('mmts').controller('defaultController', ['$scope', 'stationsServi
 	$scope.updateStation = function(type, stationModel){
 		switch(type) {
 		    case "from":
-		        sharedService.fromStation = stationModel;
+		        sharedService.updateFromStation(stationModel);
 		        break;
 		    case "to":
-		        sharedService.toStation = stationModel;
+		        sharedService.updateToStation(stationModel);
 		        break;
 		}
 		ValidateStation();
@@ -67,11 +69,26 @@ angular.module('mmts').controller('defaultController', ['$scope', 'stationsServi
 		var temp = $scope.station.from;
 		$scope.station.from = $scope.station.to;
 		$scope.station.to = temp;
+		$scope.updateStation("to", $scope.station.to);
+		$scope.updateStation("from", $scope.station.from);
+	}
+	$scope.performSearch = function(){
+		var url = '/searchtrains';
+	      $location.search('from', $scope.station.from.value).search('to', $scope.station.to.value).path(url);
 	}
 }]);
 
-angular.module('mmts').controller('searchTrainsController', ['$scope', function($scope){
-	$scope.message = "Chrome Extensions welcomes AngularJS";
+angular.module('mmts').controller('searchTrainsController', ['$scope', 'sharedService', '$routeParams',
+	function($scope, sharedService, $routeParams){
+		$scope.search = {};
+		(function(){
+			for (var i = sharedService.stations.length - 1; i >= 0; i--) {
+				if(sharedService.stations[i].value === $routeParams.from)
+					$scope.search.from = sharedService.stations[i];
+				if(sharedService.stations[i].value === $routeParams.to)
+					$scope.search.to = sharedService.stations[i];
+			};	
+		})();
 }]);
 
 angular.module('mmts').controller('liveTrainController', ['$scope', function($scope){
@@ -88,9 +105,19 @@ angular.module('mmts').service('stationsService', ['$http', '$q', function($http
 	}
 }]);
 
-angular.module('mmts').service('sharedService', function(){
-	return {
-		fromStation: null,
-		toStation: null
-	}
-});
+angular.module('mmts').factory('sharedService', ['localStorageService', 
+	function(localStorageService){
+
+	var data = {};
+	data.fromStation = localStorageService.get('from');
+	data.toStation = localStorageService.get('to');
+	data.updateFromStation =  function(from){
+		data.fromStation = from.value;
+		localStorageService.set('from',from.value);
+	};
+	data.updateToStation =  function(to){
+		data.toStation = to.value;
+		localStorageService.set('to',to.value);
+	};
+	return data;
+}]);
