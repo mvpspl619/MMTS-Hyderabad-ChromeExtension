1cc404698564880fc8d83930e7e2f534cebba10a
VenkatapathiRajuM <mvpspl619@gmail.com>
2014-08-02 21:02:45 +0530

    V 0.9

--- js/angular/app.js
+++ js/angular/app.js
@@ -20,7 +20,7 @@ angular.module('mmts', ['ngRoute', 'LocalStorageModule'])
 angular.module('mmts').controller('defaultController', ['$scope', '$location', 'stationsService', 'sharedService',
  function($scope, $location, stationsService, sharedService){
 	$scope.station = {};
-	$scope.station.isValid = false;
+	$scope.isValid = false;
 	var stationsPromise = stationsService.getStations();
 	stationsPromise.then(function(data){
 		$scope.stations = data;
@@ -47,9 +47,11 @@ angular.module('mmts').controller('defaultController', ['$scope', '$location', '
 		}
 	}	
 	function ValidateStation(){
-		$scope.station.isValid = false;
+		$scope.isValid = false;
 		if(validate($scope.station.to) && validate($scope.station.from))
-			$scope.station.isValid = true;
+			$scope.isValid = true;
+		if(_.isEqual($scope.station.to, $scope.station.from))
+			$scope.isValid = false;
 	}
 	function validate(a){
 	    return(typeof(a) != 'undefined' && a != null)
@@ -95,6 +97,11 @@ angular.module('mmts').controller('searchTrainsController', ['$scope', '$routePa
 				//show an error
 			});
 		})();
+		$scope.getClass = function(train){
+			if(train.RunsOnSunday)
+				return 'row no-left-margin no-right-margin no-bottom-margin top-margin-10 sunday';
+			return 'row no-left-margin no-right-margin no-bottom-margin top-margin-10 no-sunday';
+		}
 }]);
 
 angular.module('mmts').factory('stationsService', ['$http', '$q', 'workerService', 
@@ -109,7 +116,6 @@ angular.module('mmts').factory('stationsService', ['$http', '$q', 'workerService
 		}, function(error){
 			def.reject(error);
 		});
-		// $http.get('./data/stations.json').success(function(data){def.resolve(data)}).error(function(error){def.reject(error)});
 		return def.promise;
 	};
 	return stationsService;
@@ -117,26 +123,44 @@ angular.module('mmts').factory('stationsService', ['$http', '$q', 'workerService
 
 angular.module('mmts').factory('trainsService', ['$http', '$q', 'workerService', 'sharedService',
 	function($http, $q, workerService, sharedService){	
-
+	var sundayTrains = [47109,47110,47111,47112,47133,47134,47135,47136,47137,47158,47138,47160,47161,47179,47182,47184,47198,47199,47208,47209];
 	var trainsService = {};
 	trainsService.getTrains = function(from, to){
 		var def = $q.defer();
 		workerService.readFile('./data/trains.json')
 		.then(function(data){
-			//find the trains
+			var returningTrains = [];
 			var passingTrainNumbers = _.map(_.filter(data, function(t){return t.stationname === from.name}), function(t){return t.trainname});
 			var finalTrainNumbers = _.map(_.filter(_.filter(data, function(t){return _.contains(passingTrainNumbers, t.trainname)}), function(t){return t.stationname === to.name}), function(x){return x.trainname});
 			var initialTrains = _.filter(_.filter(data, function(d){return _.contains(finalTrainNumbers, d.trainname)}), function(e){return e.stationname === from.name});
 			var finalTrains = _.filter(_.filter(data, function(d){return _.contains(finalTrainNumbers, d.trainname)}), function(e){return e.stationname === to.name});
-			//do a resolve for list of trains
-
-			var trainsList = [];
-			def.resolve(trainsList)
+			if(finalTrains.length === initialTrains.length){
+				_.each(initialTrains, function(element, index, list){
+					var singleTrain = GetValidTrain(initialTrains[index], finalTrains[index]);
+					if(singleTrain != null)
+						returningTrains.push(singleTrain);
+				})
+			}
+			returningTrains = _.sortBy(returningTrains, 'Departure');
+			def.resolve(returningTrains);
 		}, function(error){
 			def.reject(error);
 		});
 		return def.promise;
 	};
+	var GetValidTrain = function(i, f){
+		if(i.arrivaltime < f.arrivaltime){
+			var singleTrain = {};
+			singleTrain.TrainNumber = i.trainname;
+			singleTrain.Departure = i.arrivaltime;
+			singleTrain.Arrival = f.arrivaltime;
+			singleTrain.RunsOnSunday = true;
+			if(_.contains(sundayTrains, singleTrain.TrainNumber))
+				singleTrain.RunsOnSunday = false;
+			return singleTrain;
+		}
+		return null;
+	}
 	return trainsService;
 }]);
 
@@ -166,3 +190,11 @@ angular.module('mmts').factory('sharedService', ['localStorageService','workerSe
 	};
 	return data;
 }]);
+
+angular.module('mmts').directive('result', function(){
+	return {
+		restrict: 'E',
+		replace: true,
+		templateUrl: './templates/result.html'
+	}
+});
